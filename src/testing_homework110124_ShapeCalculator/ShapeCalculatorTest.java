81d8d078fa2b0de224694a44cb7b9fb4fb09c43b
MariaKosova <kossovamaria@hotmail.com>
2024-01-14 20:09:52 +0200

    ShapeCalculatorTest added classwork

--- src/testing_homework110124_ShapeCalculator/ShapeCalculatorTest.java
+++ src/testing_homework110124_ShapeCalculator/ShapeCalculatorTest.java
@@ -28,6 +28,25 @@ class ShapeCalculatorTest {
 
         assertEquals(res, shapeCalculator.totalSquare(shapes));
     }
+
+    @Test
+    public void test_zeroShape(){
+        Shape[] actual = {};
+        assertEquals(0, shapeCalculator.totalSquare(actual));
+    }
+
+    @Test
+    public void test_oneShape(){
+        Shape[] actual = {new Rectangle(5,10)};
+        assertEquals(50.0, shapeCalculator.totalSquare(actual));
+    }
+
+    @Test
+    public void test_twoShape(){
+        Shape[] actual = {new Rectangle(5,10), new Circle(10)};
+        assertEquals(364.0, shapeCalculator.totalSquare(actual));
+    }
+
 }
 
 
