d0674095592a70b95e6ed341deca4cb3d2e11088
MariaKosova <kossovamaria@hotmail.com>
2024-01-15 10:29:42 +0200

    arrayListAdded_Main_homework1201

--- src/our_arraylist/Main.java
+++ src/our_arraylist/Main.java
@@ -5,13 +5,27 @@ public class Main {
             OurList<Integer> ourList = new OurArrayList<>();
             ourList.append(2);
             OurList<Integer>ourList1 = new OurArrayList<>();
+            ourList1.append(1);
+            ourList1.append(2);
             ourList1.append(3);
+            ourList1.append(4);
+            ourList1.append(5);
+            System.out.println( "Print list 1: " + ourList1);
             ourList1.remove(1);
             ourList1.removeById(4);
-            System.out.println(ourList1);
-            OurList<Integer> ourList2= new OurArrayList<>();
+            System.out.println("Print list 1 with changes: "+ ourList1);
+
+           OurList<Integer> ourList2= new OurArrayList<>();
+           ourList2.append(1);
+           ourList2.append(2);
+           ourList2.append(3);
+           ourList2.append(4);
+           ourList2.append(5);
+           ourList2.append(6);
+           System.out.println("Print list 2 " + ourList2);
             ourList2.removeById(1);
-            ourList2.append(2);
+            ourList2.append(7);
+            System.out.println("Print list 2 with changes: " + ourList2);
 
 
         }
